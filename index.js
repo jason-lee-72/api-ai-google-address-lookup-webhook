55b1f504694419b37c32f1667cf2b43620483f53
Jason Lee <jason.x.lee@accenture.com>
2017-07-24 13:34:14 +1000

    app and express working

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-/*
- HTTP Cloud Function.
+let express = require('express');
+let bodyParser = require('body-parser');
+let app = express();
+let port = process.env.PORT || 3000;
 
- @param {Object} req Cloud Function request context.
- @param {Object} res Cloud Function response context.
-*/
-exports.helloHttp = function helloHttp(req, res) {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use('/helloworld', (req, res) => {
   response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
 
   res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
@@ -12,4 +16,14 @@ exports.helloHttp = function helloHttp(req, res) {
     "speech": response, "displayText": response
     //"speech" is the spoken version of the response, "displayText" is the visual version
   }));
-};
+});
+
+app.use(function(req, res, next) {
+    let err = new Error('Not Found');
+    res.status = 404;
+    next(err);
+});
+
+app.listen(port, function() {
+    console.log('App listening at http://%s:%s', 'localhost', port);
+});
