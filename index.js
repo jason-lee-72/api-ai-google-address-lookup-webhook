9dfb2f87cbe0ab60ec59fb21614de183900310b6
Jason Lee <jason.x.lee@accenture.com>
2017-07-24 15:56:40 +1000

    action name

--- index.js
+++ index.js
@@ -5,26 +5,50 @@ let port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 
+function lookup(req, res) {
+  var googleMapsClient = require('@google/maps').createClient({
+    key: 'AIzaSyAsg_hoH1HVXONJhwUwR6f2fGZROJQwM1E'
+  });
+
+  googleMapsClient.geocode({
+    address: req.query.lookup
+  }, function (err, response) {
+
+    if (!err) {
+      res.json({
+        "speech": response.json.results[0].formatted_address,
+        "displayText": response.json.results[0].formatted_address
+      });
+    } else {
+       res.status(500);
+       res.send(JSON.stringify(err));
+    }
+  });
+}
+
 app.use('/webhook', (req, res) => {
-  response = "This is a sample response from your webhook! action is " + req.body.result.action;
-
-
   res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-  res.send(JSON.stringify({
-    "speech": response, "displayText": response
-    //"speech" is the spoken version of the response, "displayText" is the visual version
-  }));
+  //response = "This is a sample response from your webhook! action is " + req.body.result.action;
+
+  // switch (req.body.result.action) {
+  //   case "location.lookup":
+  lookup(req, res)
+  // }
+
+
+
+
 });
 
-app.use(function(req, res, next) {
-    let err = new Error('Not Found');
-    res.status = 404;
-    next(err);
+app.use(function (req, res, next) {
+  let err = new Error('Not Found');
+  res.status = 404;
+  next(err);
 });
 
-app.listen(port, function() {
-    console.log('App listening at http://%s:%s', 'localhost', port);
+app.listen(port, function () {
+  console.log('App listening at http://%s:%s', 'localhost', port);
 });
